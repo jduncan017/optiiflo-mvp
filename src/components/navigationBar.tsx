87a8dd38bf093c6dc8392c1369ab2724aafa72aa
jduncan017 <emailjoshduncan@gmail.com>
2024-09-23 04:19:17 -0600

    add top search bar

--- src/components/navigationBar.tsx
+++ src/components/navigationBar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useCurrentPage } from "~/contexts/currentPageContext";
 import { Bell } from "lucide-react";
 import { useSearchParams, useRouter } from "next/navigation";
+import { Input } from "~/components/ui/input";
 
 export function NavigationBar() {
   const { currentPage } = useCurrentPage();
@@ -25,20 +26,25 @@ export function NavigationBar() {
         <Button variant="ghost" size="icon" onClick={toggleSidebar}>
           <Menu className="h-7 w-7" />
         </Button>
+        <div className="flex items-center gap-5">
+          <h1 className="text-2xl font-bold">{currentPage}</h1>
+        </div>
       </div>
+      <Input
+        className="SearchBar w-full max-w-[400px] rounded-md border border-gray-300 bg-gray-100 text-black"
+        type="text"
+        placeholder="Search..."
+      />
       <div className="flex items-center gap-5">
-        <h1 className="text-2xl font-bold">{currentPage}</h1>
-      </div>
-      <div className="flex items-center gap-5">
-        <Button variant="ghost" size="icon">
-          <Bell className="h-5 w-5" />
-        </Button>
         <Image
           src="/optiiflo-logo.svg"
           alt="Optiiflo Logo"
           height={40}
           width={113}
         />
+        <Button variant="ghost" size="icon">
+          <Bell className="h-5 w-5" />
+        </Button>
       </div>
     </div>
   );
