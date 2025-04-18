cc8067bbfea2daa3d7805a1e33696c52f00e2865
jduncan017 <emailjoshduncan@gmail.com>
2025-04-18 16:52:38 -0600

    bug fix

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,11 +1,11 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import type { Database } from "@/../database.types";
+// import type { Database } from "~/database.types";
 
 export const createClient = async () => {
-  const cookieStore = await cookies();
+  const cookieStore = cookies();
 
-  return createServerClient<Database>(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
